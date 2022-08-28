6d7e359776e0c203175a72d5f99ee871b4d311f3
Alamgir <alamg9078@gmail.com>
2022-08-28 00:44:51 -0700

    Added log in api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,5 +8,6 @@ router.register('profile', views.userProfileViewSet)
 
 urlpatterns = [
    path('hello_views/', views.HelloAPIView.as_view() ),
+   path('login/', views.UserLoginApiView.as_view()),
    path('', include(router.urls))
 ]
